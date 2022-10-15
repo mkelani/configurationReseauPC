020d25b57096045e60a95e263e9f296b22251e9b
Maxime Kélani <maxime.kelani.pro@gmail.com>
2022-10-15 14:04:33 +0200

    Delete -e of the echo lign 5

--- configurationReseauInteractif.sh
+++ configurationReseauInteractif.sh
@@ -2,7 +2,7 @@
 
 echo "Vous devez rentrer successivement les informations suivantes : Nom du PC, adresse mac, addresse IP : "
 read -p "Nom du PC : " pc ; read -p "adresse mac : " mac ; read -p "addresse IP : " ip 
-echo -e $pc $mac $ip >> configurationReseau.txt
+echo $pc $mac $ip >> configurationReseau.txt
 echo "Merci d'avoir entré les informations suivantes : "
 echo "Nom du PC : $pc" ; echo "Adresse MAC : $mac" ; echo "Adresse IP : $ip"
 exit 0
