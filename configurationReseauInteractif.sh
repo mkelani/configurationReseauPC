995e7574a7eda66e9dba83c39758f672b810d2e8
Maxime Kélani <maxime.kelani.pro@gmail.com>
2022-10-15 11:55:19 +0200

    Add the txt and bash files to the project

---
+++ configurationReseauInteractif.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "Vous devez rentrer successivement les informations suivantes : Nom du PC, adresse mac, addresse IP : "
+read -p "Nom du PC : " pc ; read -p "adresse mac : " mac ; read -p "addresse IP : " ip 
+echo -e $pc $mac $ip "\n" >> configurationReseau.txt
+echo "Merci d'avoir entré les informations suivantes : "
+echo "Nom du PC : $pc" ; echo "Adresse MAC : $mac" ; echo "Adresse IP : $ip"
+exit 0
